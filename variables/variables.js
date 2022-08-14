ff5fb80772f2fad90442da1cb11adf5c513cc753
Rafael Santos <contactrafaelsantos@gmail.com>
2022-08-14 10:02:02 -0300

    Using variables and constants

---
+++ variables/variables.js
@@ -0,0 +1,19 @@
+let message = "Potato";
+console.log(message);
+
+//multiple variables in one line
+let name = "Rafael", 
+    age = 584, 
+    from = "Brazil";
+
+//declare two variables and copy one to another
+let one = 1;
+let two = 2;
+
+one = two;
+console.log(one);
+
+//constant variable uses const keyword instead let
+const myBirthday = "23.05.2000"
+//myBirthday = "abc" -> error
+console.log(myBirthday);
