15c7bda2936bb07e5783886fcd727813be380bd9
Rafael Santos <contactrafaelsantos@gmail.com>
2022-08-24 16:50:25 -0300

    loops

---
+++ loops/while_loop_and_for_loop.js
@@ -0,0 +1,29 @@
+let i = 0
+//while the contidion is true, the code block will be executed
+while(i < 3) {
+    console.log(i);
+    i++ //the value must be incremented
+}
+
+//decrementing a value
+console.log("===========")
+i = 3
+while(i) {
+    console.log(i)
+    i--;
+}
+
+//do while
+console.log("=========")
+i = 0
+do{
+    console.log(i)
+    i++
+} while(i < 5)
+
+//for loop
+console.log("===for loop===")
+
+for(let i = 0; i < 5; i++) {
+    console.log(i)
+}
