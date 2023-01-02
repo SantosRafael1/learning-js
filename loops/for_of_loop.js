74641d715875e6a65700af54c1e6d1b73803d15e
Rafael Santos <105432709+SantosRafael1@users.noreply.github.com>
2023-01-02 20:37:40 -0300

    for-of loop

---
+++ loops/for_of_loop.js
@@ -0,0 +1,11 @@
+// for of loop
+
+favoriteSubjects = ["CS", "Physics", "Biology"];
+for (const subjects of favoriteSubjects) {
+    console.log(subjects);
+}
+
+//Como funciona:
+//1. Obtém o primeiro item do array
+//2. O item será armazenado na variável 'subjects' e executar o código entre chaves
+//3. A variável então receberá o próximo item e repetir o código até alcançar o final do array
